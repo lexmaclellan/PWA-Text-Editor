4ae9036be991f1454e6bced2905b14fe48ef318a
lexrayne <30938032+lexrayne@users.noreply.github.com>
2023-10-13 22:06:17 -0300

    Added getDB function

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,14 +1,14 @@
 import { openDB } from 'idb';
 
 const initdb = async () =>
-  openDB('jate', 1, {
+  openDB('textdb', 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+      if (db.objectStoreNames.contains('textdb')) {
+        console.log('textdb database already exists');
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      db.createObjectStore('textdb', { keyPath: 'id', autoIncrement: true });
+      console.log('textdb database created');
     },
   });
 
@@ -24,6 +24,25 @@ export const putDb = async (content) => {
 };
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.log('GET all from the database');
+
+  // Create a connection to the database database and version we want to use.
+  const textDb = await openDB('textdb', 1);
+
+  // Create a new transaction and specify the database and data privileges.
+  const tx = textDb.transaction('textdb', 'readonly');
+
+  // Open up the desired object store.
+  const store = tx.objectStore('textdb');
+
+  // Use the .getAll() method to get all data in the database.
+  const request = store.get(1);
+
+   // Get confirmation of the request.
+  const result = await request;
+  console.log('result.value', result);
+  return result.value;
+}
 
 initdb();
